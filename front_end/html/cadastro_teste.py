f713b40a6af39726e77185a74c9c9c5f12800ca9
rregorr <rregorr@gmail.com>
2023-10-20 22:17:50 -0300

    python depois de 2 semanas

--- front_end/html/cadastro_teste.py
+++
@@ -1,8 +0,0 @@
-lista = ["arroz", "feijao"]
-
-lista.append("mandioca")
-
-lista.insert(1, "milho")
-
-print(lista)
-print("")
---
+++ python/python_tutorial.py
@@ -0,0 +1,8 @@
+#Sequência para upar uma alteração (1 - git add, 2 - git commit 3 - git push)
+#Desempacotando coleções - permite extrair valores de coleções em variáveis
+frutas = ["Morango", "Maças", "Laranjas"]
+x, y, z = frutas
+
+print(x)
+print(y)
+print(z)
