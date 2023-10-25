323e92bd40794a1956d88c7af467e4ddf631f240
rregorr <rregorr@gmail.com>
2023-10-25 15:29:25 -0300

    Finalizada Tarefa 171

--- javinha/Cadastro.java
+++ javinha/Cadastro.java
@@ -1,10 +1,29 @@
+import java.util.ArrayList;
+
 public class Cadastro {
   public static void main(String[] args) {
-    String[] nomes = {"Rogerio", "Lorenna" , "Java", "Heitor"};
+    //Array não podem ser alterados em tempo de execução
+    String[] nomes_array = {"Rogerio", "Lorenna" , "Java", "Heitor"};
 
-    System.out.println("Nome da família: ");
-    for (String nome : nomes) {
-      System.out.println(nome+"\n");
+    System.out.println("\nNome da família usando Array: ");
+    for (String nome : nomes_array) {
+      System.out.println(nome);
+    }
+    System.out.println("\nNome da família usando ArrayList: ");
+    //Para suprir o problema dos Arrays, usamos Arraylists
+    ArrayList<String> nomes_arraylist = new ArrayList<>();
+    nomes_arraylist.add("Rogerio Lima");
+    nomes_arraylist.add("Java Maria");
+
+    int[][] idades = {{10,20},{20,30}};
+
+    for (String nome_arraylist : nomes_arraylist) {
+       System.out.println(nome_arraylist);      
+    }
+    System.out.printf("%5d%30d%n", 5, 10);
+    
+    for (int[] idade : idades) {
+      System.out.println("Idade no Array Bidimensional: " + idade);
     }
   }
 }
