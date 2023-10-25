2acd625befc944ed44a5114bdb9321a04eda3f6d
rregorr <rregorr@gmail.com>
2023-10-25 18:25:06 -0300

    App js-nodejs-react-html-css definida

---
+++ js/meu-projeto/src/App.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import './App.css';
+import './Login.css';
+
+
+function App() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(`Email: ${email}, Password: ${password}`);
+  };
+
+  return (
+    <div className="App">
+      <h1>Login</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
+
