5e26e26b7c3b6ac9c4ea1f99c7f5b7530992f9af
rregorr <rregorr@gmail.com>
2023-12-06 22:14:21 -0300

    bacen

--- java/Fibonacci.java
+++ java/Fibonacci.java
@@ -1,38 +1,41 @@
+package src.main.java;
 import java.util.Scanner;
 
 public class Fibonacci {
- public static void main(String[] args) {
-
- int a = 0, b = 1, c = 0, contador = 2;
- System.out.println("Forneça um número inteiro maior que zero");
- Scanner sc = new Scanner(System.in);
- int numero = sc.nextInt();
-
-while (contador <= numero) {
- if (numero > 0) {
-  if (numero == 1) {
-   System.out.println(0);
-  }
-  else if (numero == 2) {
-   System.out.println(1);
-  } 
+  public static long CalculaFibonacci(int z) {
+    System.out.println("Informe um número: ");
+    Scanner sc = new Scanner(System.in);
+    long numero = sc.nextLong();
   
-  else {
-   a = 0;
-   b = 1;
-   c = a + b;
-   a = b;
-   contador++;
-   System.out.println(c);
+    long cont = 0;
+    long a = 0;
+    long b = 1;
+    long c;
+
+    if (numero == 0) {
+        System.out.println("A sequência Fibonacci para o número " + numero + " é: 0");
+    }
+
+    else if (numero == 1) {
+        System.out.println("A sequência Fibonacci para o número " + numero + " é: 01");
+    }
+
+    else {
+      System.out.println("A sequência Fibonacci para o número " + numero + " é: \n0\n1");
+
+      while (cont <= numero) {
+        c = a + b; 
+        a = b; 
+        b = c;
+        cont++;
+        
+        System.out.println(b);
+    }
+    }
+    return b;
   }
- }
+  public static void main (String[] args) {
+    CalculaFibonacci(20);
+    
+  }
 }
-}
-}
-
-/* https://wagnergaspar.com/exercicio-5-imprimir-o-enesimo-termo-da-sequencia-de-fibonacci-com-fluxograma/
-1 - a = 0; b = 1; c = a + b (0+1=1); 
-
-a = b; b = c;;;
-
-*/
