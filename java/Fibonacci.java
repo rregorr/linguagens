2d340050ea10fa57f683c245962e0394269cd414
ROGERIO LIMA CARNEIRO <67436528220@trt8.jus.br>
2023-12-12 13:47:42 -0300

    java comments

--- java/Fibonacci.java
+++ java/Fibonacci.java
@@ -2,30 +2,34 @@ package src.main.java;
 import java.util.Scanner;
 
 public class Fibonacci {
-  public static long CalculaFibonacci(int z) {
+  public static long CalculaFibonacci(int n) {
     
     long cont = 0;
     long a = 0;
     long b = 1;
     long c;
 
-    if (z == 0) {
-        System.out.println("A sequência Fibonacci para o número " + z + " é: 0");
+    if (n < 0) {
+	System.out.println("A sequência Fibonacci não admite números negativos");
+    }	
+
+    if (n == 0) {
+        System.out.println("A sequência Fibonacci para o número " + n + " é: 0");
     }
 
-    else if (z == 1) {
-        System.out.println("A sequência Fibonacci para o número " + z + " é: 01");
+    else if (n == 1) {
+        System.out.println("A sequência Fibonacci para o número " + n + " é: 0, 1");
     }
 
     else {
-      System.out.println("A sequência Fibonacci para o número " + z + " é: \n0\n1");
+      System.out.println("A sequência Fibonacci para o número " + n + " é: \n0\n1");
 
-      while (cont <= z) {
+      while (cont < n) {
+        cont++;
         c = a + b; 
         a = b; 
         b = c;
-        cont++;
-        
+       
         System.out.println(b);
     }
     }
@@ -39,4 +43,4 @@ public class Fibonacci {
     CalculaFibonacci(numero);
     
   }
-}//
+}
