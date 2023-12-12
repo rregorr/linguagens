3b63887ef621574e5fdaa1db29ada4847f28abe1
ROGERIO LIMA CARNEIRO <67436528220@trt8.jus.br>
2023-12-12 13:35:17 -0300

    java comments

--- java/Fibonacci.java
+++ java/Fibonacci.java
@@ -3,27 +3,24 @@ import java.util.Scanner;
 
 public class Fibonacci {
   public static long CalculaFibonacci(int z) {
-    System.out.println("Informe um número: ");
-    Scanner sc = new Scanner(System.in);
-    long numero = sc.nextLong();
-  
+    
     long cont = 0;
     long a = 0;
     long b = 1;
     long c;
 
-    if (numero == 0) {
-        System.out.println("A sequência Fibonacci para o número " + numero + " é: 0");
+    if (z == 0) {
+        System.out.println("A sequência Fibonacci para o número " + z + " é: 0");
     }
 
-    else if (numero == 1) {
-        System.out.println("A sequência Fibonacci para o número " + numero + " é: 01");
+    else if (z == 1) {
+        System.out.println("A sequência Fibonacci para o número " + z + " é: 01");
     }
 
     else {
-      System.out.println("A sequência Fibonacci para o número " + numero + " é: \n0\n1");
+      System.out.println("A sequência Fibonacci para o número " + z + " é: \n0\n1");
 
-      while (cont <= numero) {
+      while (cont <= z) {
         c = a + b; 
         a = b; 
         b = c;
@@ -35,7 +32,11 @@ public class Fibonacci {
     return b;
   }
   public static void main (String[] args) {
-    CalculaFibonacci(20);
+    System.out.println("Informe um número: ");
+    Scanner sc = new Scanner(System.in);
+    int numero = sc.nextInt();
+  
+    CalculaFibonacci(numero);
     
   }
-}
+}//
