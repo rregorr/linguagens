c3cded7a6332dc7c525b143146798d3a57addb1e
ROGERIO LIMA CARNEIRO <67436528220@trt8.jus.br>
2024-01-24 16:09:32 -0300

    python-java

--- python/OperacoesBasicas.py
+++ python/OperacoesBasicas.py
@@ -1,9 +1,16 @@
-def soma(a, b):
-    return print(f"O valor de a mais b eh: ", a + b)
+class OperacoesBasicas:
+    def soma(self, a, b):
+        return a + b
 
-def a_potencia_b(a, b):
-    a_potencia_b = a**b
-    return a_potencia_b
+    def a_potencia_b(self, a, b):
+        a_potencia_b = a**b
+        return a_potencia_b
 
-soma(4,5)
-a_potencia_b(4,4)
+# Crie uma instância da classe
+operacoes_basicas = OperacoesBasicas()
+# Chame os métodos usando a instância
+resultado_soma = operacoes_basicas.soma(4, 5)
+resultado_potencia = operacoes_basicas.a_potencia_b(4, 4)
+
+print(resultado_soma)  # Irá imprimir 9
+print(resultado_potencia)  # Irá imprimir 16
