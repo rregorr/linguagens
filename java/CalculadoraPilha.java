89f4f02af10bdd2adf9b37580efb91ab46dda95d
rregorr <rregorr@gmail.com>
2025-07-15 20:31:35 -0300

    pilha

--- java/CalculadoraPilha.java
+++ java/CalculadoraPilha.java
@@ -10,84 +10,83 @@ Inserção no final	O(1)* ou O(n)
 *Se a lista for duplamente encadeada com ponteiro para o final, a inserção no final pode ser O(1). 
  Caso contrário, é O(n) porque precisa percorrer.
  */
+import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
 import java.util.Scanner;
+import java.util.Stack;
 
 public class CalculadoraPilha {
     Scanner sc = new Scanner(System.in);
-    ArrayList<Integer> armazenaNumeros = new ArrayList<>();
+    Deque<Integer> numeros = new LinkedList<>(); 
 
-    public int somar(int num1, int num2, int num3, int num4, int num5, int num6, int num7) {
-        num1 = 3; num2 = 5; num3 = 7; num4 = 11; num5 = 13; num6 = 17; num7 = 19;
-
-        int soma = num1 + num2 + num3 + num4 + num5 + num6 + num7;
-        armazenaNumeros.add(num1); armazenaNumeros.add(num2);
-        armazenaNumeros.add(num3);
-        armazenaNumeros.add(num4);
-        armazenaNumeros.add(num5);
-        armazenaNumeros.add(num6);
-        armazenaNumeros.add(num7);
-        return soma;
+    public Deque<Integer> empilhar () {
+        System.out.println("Informe próximo número da pilha ou -1 para sair!");
+        int num = sc.nextInt();
+        
+        while (num != -1) {
+            numeros.push(num);
+        System.out.println("Informe próximo número da pilha ou -1 para sair!");
+            num = sc.nextInt();
+        }
+        System.out.println("Apos o empilhamento, Agora a lista contém os seguintes elementos: ");
+        return numeros;
     }
     //Método que remove o número do topo da lista
-    public int removerTopo() {
-        return armazenaNumeros.remove(armazenaNumeros.size()-1);
+    public Deque<Integer> removerTopo() {
+        int topo = numeros.pop();
+        System.out.println("Apos a remoção do topo ("+topo+"), a lista contém os seguintes elementos: ");
+        return numeros;
     }
 
-    //Método que remove o número correspondente ao indice informado da lista
-    public int removerIndice(int indice) {
-    if (indice >= 0 && indice < armazenaNumeros.size()) {
-        return armazenaNumeros.remove(indice);
-    } else {
-        System.out.println("Índice inválido.");
-        return -1;
-    }
-}
     //Método que remove o número informado, caso exista na lista
-    public int removerNumero(int numero) {
-    if (armazenaNumeros.contains(numero)) {
-        armazenaNumeros.remove(Integer.valueOf(numero));
-        System.out.println(numero);
-        return numero;
+    public Deque<Integer> removerNumero() {
+        System.out.println("Informe o número que quer remover:");
+        int numeroRemover = sc.nextInt();
+    if (numeros.contains(numeroRemover)) {
+        boolean removido = numeros.remove(Integer.valueOf(numeroRemover)); // Remove o objeto Integer
+        if (removido) {
+            System.out.println(numeroRemover + " removido com sucesso!");
+
+        } else {
+            System.out.println("Erro ao remover!");
+
+        }
+
     } else {
         System.out.println("Número não encontrado na lista.");
-        return -1;
+
     }
-}
-    //Método que retorna o número cujo índice é informado, caso exista na lista
- public int retornaIndice(int indice) {
-    if (indice >= 0 && indice < armazenaNumeros.size()) {
-        return armazenaNumeros.get(indice);
-    } else {
-        System.out.println("Índice inválido.");
-        return -1; // valor padrão de erro
-    }
+    System.out.println("Apos a remoção de " + numeroRemover + ", Agora a lista contém os seguintes elementos: ");
+    return numeros;
 }
 
+public int somarPilha() {
+    int totalPilha = 0;
+    for (int num : numeros) {
+        totalPilha += num;
+    }
+    System.out.println("A soma de todos os números na pilha é: " + totalPilha);
+    return totalPilha;
+}
+   
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        Calculadora calculadora = new Calculadora();
-        int soma = calculadora.somar(1,1,1,1,1,1,1);
-        System.out.println(soma);
-        System.out.println("Lista de números completa: " + calculadora.armazenaNumeros);
+        CalculadoraPilha calculadora = new CalculadoraPilha();
+        
+        //Empilhar
+        calculadora.empilhar();
+        
+        //Somar
+        calculadora.somarPilha();
 
-        int removeTopo = calculadora.removerTopo();
-        System.out.println("Número do topo removido: " + removeTopo);
-        System.out.println("Agora a lista contém os seguintes elementos: " + calculadora.armazenaNumeros);
-        System.out.println("Informe um índice, cujo número deseja remover:");
-        int indice = sc.nextInt();
-        calculadora.removerIndice(indice);
-        System.out.println("A lista agora contém os números: " + calculadora.armazenaNumeros);
+       //Remover topo
+       calculadora.removerTopo();
+        
+        //Remover um número
+        calculadora.removerNumero();
 
-        System.out.println("Informe um número que deseja remover:");
-        int numero = sc.nextInt();
-        calculadora.removerNumero(numero);
-        System.out.println("A lista agora contém os números: " + calculadora.armazenaNumeros);
-
-        System.out.println("Informe um indice para retornar o número correpondente:");
-        int indice2 = sc.nextInt();
-        int retornaIndice = calculadora.retornaIndice(indice2);
-        System.out.println("O número correspondente ao índice informado é: " + retornaIndice);
     }
 
+    
 }
