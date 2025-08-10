0de4392bce66f468e755f17c4dc59131f3267ac6
rregorr <rregorr@gmail.com>
2025-08-09 23:04:45 -0300

    taxas

--- js/matematica/taxaJuros.js
+++ js/matematica/taxaJuros.js
@@ -7,11 +7,28 @@
 
 
 
- 
+
  /* Taxa de Juros Efetiva
  A taxa de juros efetiva é aquela que considera a capitalização dos juros em um determinado período
  de tempo.  Ela reflete o custo real do dinheiro, pois leva em conta a frequência com que os juros são  
  capitalizados. 
  */ 
+function taxaJurosEfetiva (N, i, t) {
+    let taxaEfetiva = (1 + i) ** t - 1;
+    return taxaEfetiva;
+}       
+let taxaEfetiva = taxaJurosEfetiva(1000, 0.01, 12);
+console.log(`\nTaxa de Juros Efetiva: ${taxaEfetiva.toFixed(4)}`);  
 
+function conversaoRealAparente(Ia, Inf) {
+    /*
+    Ia: Taxa de juros aparente
+    Iinf: Taxa de inflação
+    Ir: Taxa de juros real
+    */
+    //(1+Ia) = (1+Inf)*(1+Ir);
+    let Ir = ((Ia - Inf) / (1 + Inf));
+    return Ir;
+}
+console.log("\nTaxa de Juros Real: " + conversaoRealAparente(0.8, 0.2));    
 
