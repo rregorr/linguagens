5cbae3d7e9d6d0941ddade36ee96d530d1495392
rregorr <rregorr@gmail.com>
2025-08-11 19:11:25 -0300

    fazendo alteracoes

--- git_atualiza.bat
+++ git_atualiza.bat
@@ -1,5 +1,5 @@
 @echo off
 call git add .
-call git commit -m "fazendo alterações"
+call git commit -m "fazendo alteracoes"
 call git push origin main
-echo Foram executados os seguintes comandos: git add ., git commit -m "fazendo alterações" e git push origin main
+echo Foram executados os seguintes comandos: git add ., git commit -m "fazendo alteracoes" e git push origin main
